4884d692972b2c0c91bf95d5dd49f9c68f21e9e4
elijahIQ <146786006+elijahIQ@users.noreply.github.com>
2025-10-15 20:32:32 -0700

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000/hostels";
+
 const API_URL = "https://ucc-campus-hostel-backend.onrender.com/hostels";
 
 async function fetchHostels() {
@@ -26,4 +26,5 @@ async function fetchHostels() {
 }
 
 fetchHostels();
+
 
